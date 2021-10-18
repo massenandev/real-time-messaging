b0a44b3c7596d82cdd68b0fa8723c2ae84d12245
massenandev <massenandev@gmail.com>
2021-10-18 20:13:31 -0300

    feat(github): set github OAuth and github login route

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,17 @@
+// pra ter acesso às var de ambiente
+import 'dotenv/config'
 import express from 'express'
 
 const app = express()
+
+app.get('/github', (request, response) => {
+  response.redirect(`https://github.com/login/oauth/authorize?client_id=${process.env.GITHUB_CLIENT_ID}`)
+})
+
+app.get('/signin/callback', (request, response) => {
+  const { code } = request.query
+
+  return response.json(code)
+})
 
 app.listen(4000, () => console.log('Server is running on port 4000'))
