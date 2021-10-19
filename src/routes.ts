586b0965e475b6603135a249359db9b84df95e67
massenandev <massenandev@gmail.com>
2021-10-18 22:04:55 -0300

    feat(user): create user profile

--- src/routes.ts
+++ src/routes.ts
@@ -2,6 +2,7 @@ import { Router } from 'express'
 import { AuthenticateUserController } from './controllers/AuthenticateUserController'
 import { CreateMessageController } from './controllers/CreateMessageController'
 import { GetLastThreeMessagesController } from './controllers/GetLastThreeMessagesController'
+import { ProfileUserController } from './controllers/ProfileUserController'
 import { ensureAuthenticated } from './middlewares/ensureAuthenticates'
 
 const router = Router()
@@ -9,6 +10,8 @@ const router = Router()
 router.post('/authenticate', new AuthenticateUserController().handle)
 router.post('/messages', ensureAuthenticated, new CreateMessageController().handle)
 router.get('/messages/lastthree', new GetLastThreeMessagesController().handle)
+router.get('/profile', ensureAuthenticated, new ProfileUserController().handle)
+
 
 
 export { router }
