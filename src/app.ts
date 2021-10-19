f0f346d2233aff95339866b924d95091fa0ea733
massenandev <massenandev@gmail.com>
2021-10-18 21:51:33 -0300

    feat(messages): add socket.io

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,35 @@
 // pra ter acesso às var de ambiente
 import 'dotenv/config'
 import express from 'express'
+import http from 'http'
+//responsavel por permitir ou barrar requisições dentro da aplicação
+// se eu quero outras fontes pra se conectar com a aplicação, deve-se habilitar o cors
+import cors from 'cors'
+
+import { Server } from 'socket.io'
 
 import { router } from './routes'
 
 const app = express()
+//habilita o cors pro app
+app.use(cors())
+
+// quando subir o serverhttp, o app tb sobe junto
+const serverHttp = http.createServer(app)
+
+//acesso a conexao do cliente
+// permite que outras fontes se conectem
+const io = new Server(serverHttp, {
+  cors: {
+    origin: "*"
+  }
+})
+
+//ouvindo evento de conecxão pra saber se algum user se conectou na aplicação
+io.on("connection", socket => {
+  console.log(`Usuário conectado no docker ${socket.id}`)
+})
+
 app.use(express.json())
 
 app.use(router)
@@ -19,4 +44,4 @@ app.get('/signin/callback', (request, response) => {
   return response.json(code)
 })
 
-app.listen(4000, () => console.log('Server is running on port 4000'))
+export { serverHttp, io }
